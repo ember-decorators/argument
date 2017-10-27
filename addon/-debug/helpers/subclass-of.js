5582e87b2d977b1e63919f5864e815193553d40f
Chris Garrett <me@pzuraq.com>
2017-10-26 20:18:17 -0700

    bugfix(primitives): Adds primitive tests and fixes some primitive validators (#15)

--- addon/-debug/helpers/subclass-of.js
+++ addon/-debug/helpers/subclass-of.js
@@ -1,10 +1,9 @@
 import { assert } from '@ember/debug';
 import { makeValidator } from '../utils/validators';
-import isClass from '../utils/is-class';
 
 export default function subclassOf(superclass) {
   assert(`The 'subclassOf' helper must receive exactly one class`, arguments.length === 1);
-  assert(`The 'subclassOf' helper must receive a class constructor, received: ${superclass}`, isClass(superclass));
+  assert(`The 'subclassOf' helper must receive a class constructor, received: ${superclass}`, typeof superclass === 'function');
 
   return makeValidator(`subclassOf(${superclass})`, (value) => {
     return (
