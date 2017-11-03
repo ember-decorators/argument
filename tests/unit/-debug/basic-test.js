eefcd681ad019516657f4541fc6a08eff4ee3b72
Christopher Garrett <me@pzuraq.com>
2017-11-03 11:34:35 -0700

    add test

---
+++ tests/unit/-debug/basic-test.js
@@ -0,0 +1,12 @@
+import EmberObject from '@ember/object';
+import { test, module } from 'qunit';
+
+module('basic tests');
+
+test('validations do not cause errors on unvalidated objects' , function(assert) {
+  class Foo extends EmberObject {}
+
+  const foo = Foo.create({ bar: 'baz' });
+
+  assert.equal(foo.get('bar'), 'baz', 'everything works correctly');
+});
