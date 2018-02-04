d55fba7ad07e1fc1cfbe32706def35316faa9592
Christopher Garrett <me@pzuraq.com>
2018-02-04 10:32:43 -0800

    bugfix(validations): Fixes validations in legacy

--- tests/unit/component/component-test.js
+++ tests/unit/component/component-test.js
@@ -1,4 +1,5 @@
 import Component from '@ember/component';
+import { addObserver } from '@ember/object/observers';
 
 import hbs from 'htmlbars-inline-precompile';
 import { moduleForComponent } from 'ember-qunit';
@@ -56,4 +57,93 @@ test('argument value can be overridden in subclass', function(assert) {
   this.render(hbs`{{foo-component id="bar"}}`);
 
   assert.equal(find('#bar').textContent, '123');
+});
+
+test('argument works with bindings', function(assert) {
+  assert.expect(1);
+
+  let calls = [];
+
+  class FooComponent extends Component {
+    @argument foo = true;
+
+    didInsertElement() {
+      addObserver(this, 'foo', () => {
+        calls.push(this.get('foo'));
+      });
+    }
+  }
+
+  this.register('component:foo-component', FooComponent);
+
+  this.render(hbs`{{foo-component foo=bar}}`);
+
+  this.set('bar', false);
+  this.set('bar', true);
+  this.set('bar', false);
+  this.set('bar', false);
+  this.set('bar', true);
+  this.set('bar', 123);
+  this.set('bar', 123);
+
+  assert.deepEqual(calls, [false, true, false, true, 123], 'binding updated correctly');
+});
+
+test('argument defaultIfUndefined works with bindings', function(assert) {
+  assert.expect(1);
+
+  let vals = [];
+
+  class FooComponent extends Component {
+    @argument({ defaultIfUndefined: true })
+    foo = true;
+
+    didInsertElement() {
+      vals.push(this.get('foo'));
+
+      addObserver(this, 'foo', () => vals.push(this.get('foo')));
+    }
+  }
+
+  this.set('foo', undefined);
+
+  this.register('component:foo-component', FooComponent);
+
+  this.render(hbs`{{foo-component foo=foo}}`);
+
+  this.set('foo', 123);
+  this.set('foo', undefined);
+
+  assert.deepEqual(vals, [true, 123, true]);
+});
+
+test('argument defaultIfNullish works with bindings', function(assert) {
+  assert.expect(1);
+
+  let vals = [];
+
+  class FooComponent extends Component {
+    @argument({ defaultIfNullish: true })
+    foo = true;
+
+    @argument({ defaultIfNullish: true })
+    bar = true;
+
+    didInsertElement() {
+      vals.push(this.get('foo'));
+
+      addObserver(this, 'foo', () => vals.push(this.get('foo')));
+    }
+  }
+
+  this.set('foo', undefined);
+
+  this.register('component:foo-component', FooComponent);
+
+  this.render(hbs`{{foo-component foo=foo}}`);
+
+  this.set('foo', 123);
+  this.set('foo', null);
+
+  assert.deepEqual(vals, [true, 123, true]);
 });
