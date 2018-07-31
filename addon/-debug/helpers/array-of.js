8ca0770c6bd2d9b95693cf80c02e90cc8a062dc7
Miguel Andrade <jmandrade0@gmail.com>
2018-07-31 02:16:01 +0100

    Use ember's isArray on arrayOf validator

--- addon/-debug/helpers/array-of.js
+++ addon/-debug/helpers/array-of.js
@@ -1,5 +1,6 @@
 import { assert } from '@ember/debug';
 import { resolveValidator, makeValidator } from '../utils/validators';
+import { isArray } from '@ember/array';
 
 export default function arrayOf(type) {
   assert(`The 'arrayOf' helper must receive exactly one type. Use the 'unionOf' helper to create a union type.`, arguments.length === 1);
@@ -7,7 +8,7 @@ export default function arrayOf(type) {
   const validator = resolveValidator(type);
 
   return makeValidator(`arrayOf(${validator})`, (value) => {
-    return Array.isArray(value) && value.every(validator);
+    return isArray(value) && value.every(validator);
   });
 }
 
